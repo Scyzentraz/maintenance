aec249fc8f40a2acdde50198b14ea1a90838bee5
Scyzentraz <scyzentraz@gmail.com>
2024-11-15 17:51:33 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const fadeInElements = document.querySelectorAll(".fade-in");
-
-  fadeInElements.forEach((el, index) => {
-    el.style.animationDelay = `${index * 0.5}s`;
-    el.classList.add("visible");
-  });
-});
